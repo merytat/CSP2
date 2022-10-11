28e6803998161607881a8c1ef2a9299029fb724b
mtellezjis <70731290+mtellezjis@users.noreply.github.com>
2022-10-11 15:00:33 +0700

    thu

---
+++ src/Unit2/Example.java
@@ -0,0 +1,33 @@
+package Unit2;
+
+public class Example {
+    //method1
+    public void method1(int a){
+    }
+
+    public boolean method2(){
+    }
+    
+    private String method3(int a){
+    }
+
+    public void method4(String name){
+    }
+
+    public double method5(){
+    }
+
+    public int method6(boolean isStudent){
+    }
+}
+
+class Tester{
+    public static void main(String[] args) {
+        Example e1 = new Example();
+
+        e1.method4("Mery");
+        double var1 = e1.method5();
+        System.out.println(e1.method6(true));
+        int var2 = e1.method6(false);
+    }
+}
