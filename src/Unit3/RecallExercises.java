bac51b33d39356957cb18b96b7fe8a2442d17d89
mtellezjis <mtellez@jisedu.or.id>
2023-01-10 21:35:22 +0700

    ad

---
+++ src/Unit3/RecallExercises.java
@@ -0,0 +1,15 @@
+package Unit3;
+
+public class RecallExercises {
+
+    public ... method1{
+
+    }
+}
+
+class RecallTester{
+    public static void main(String[] args) {
+        RecallExercises re = new RecallExercises();
+        re.method1("ooableble");
+    }
+}
